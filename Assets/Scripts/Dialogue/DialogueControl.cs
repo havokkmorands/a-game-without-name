a3434d8f4dcbce0b98df5825ac50f489d196b921
havokkmorands <bruno@novo.com>
2024-08-13 19:51:37 -0300

    :pencil: First Commit - Project in its early stages.

---
+++ Assets/Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+
+    [Header("Components")]
+    public GameObject dialogueObj;
+    public Image profileSprite;
+    public Text speechText;
+    public Text actorNameText;
+
+    [Header("Settings")]
+    public float typingSpeed;
+
+    private bool isShowing;
+    private int index;
+
+    private string[] sentences;
+
+    public static DialogueControl instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+
+    }
+
+    public void Speech(string[] txt)
+    {
+        if (!isShowing)
+        {
+            dialogueObj.SetActive(true);
+            sentences = txt;
+            StartCoroutine(TypeSentence());
+            isShowing = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/NPC/NPC_Dialogue.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Dialogue : MonoBehaviour
+{
+    public float dialogueRange;
+    public LayerMask playerLayer;
+
+    public DialogueSettings dialogue;
+
+    private List<string> sentences = new List<string>();
+
+    bool playerHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetNPCInfo();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerHit)
+        {
+            DialogueControl.instance.Speech(sentences.ToArray());
+        }
+    }
+
+    void GetNPCInfo()
+    {
+        for (int i = 0; i < dialogue.dialogues.Count; i++)
+        {
+            sentences.Add(dialogue.dialogues[i].sentence.portuguese);
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ShowDialogue();
+    }
+    void ShowDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+
+        if (hit != null)
+        {
+            // Debug.Log("Player na área, derrubou é penalti");
+            playerHit = true;
+        }
+        else
+        {
+            playerHit = false;
+            DialogueControl.instance.dialogueObj.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
+    }
+}
